40c4c945e313cb21b9a852e909b836bd7173aec3
NazarVolynko <nazarvolynko@meta.ua>
2019-12-24 15:37:05 +0200

    lab_9_nazar

---
+++ dk72_volynko/lab9_cache/src/reg32.sv
@@ -0,0 +1,22 @@
+// code part take from Oleh Matusha
+
+`timescale 1ns/1ps
+
+module register #(parameter WIDTH = 32) (clk, we, d, q, arst);
+	input clk,we,arst;
+	input [WIDTH-1:0] d;
+	output reg [WIDTH-1:0] q;
+
+	
+
+	always @(posedge clk or negedge arst) begin 
+		if(!arst) begin 
+			q <= '0;
+		end else if(we) begin
+			q <= d;
+		end else begin 
+			q <= q;
+		end 
+	end
+
+endmodule 
